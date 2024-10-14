d40fdb0ff8e376adca5d9d8be1e74741d9542686
Roosch <85234531+enzorooschqueiroz@users.noreply.github.com>
2024-10-14 12:09:49 -0300

    :sparkles: Introducing FLASK RESTFUL

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Assignments(Resource):
+    def get(self):
+        return {'message': "assignment"}
+
+
+class Assignment(Resource):
+    def post(self):
+        return {'message': "assignment created"}
+
+    def get(self, asssingment_id):
+        return {'message': "assignment 1"}
+
+
+api.add_resource(Assignments, '/assignments')
+api.add_resource(Assignment, '/assignment', 'assignment/<int:assignment_id>')
