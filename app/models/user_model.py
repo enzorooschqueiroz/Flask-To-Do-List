3041f2ffbc28e4bc11e0661f5133f97da1a4b859
Roosch <85234531+enzorooschqueiroz@users.noreply.github.com>
2024-10-15 10:14:14 -0300

    :broom: cleanup: useless comments

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -7,7 +7,6 @@ class UserModel(Document):
     user_email = EmailField(required=True, unique=True)
     user_password = StringField(required=True)
     
-    # Se você quiser que o MongoDB armazene uma lista de IDs de tarefas associadas ao usuário
     user_assignments = ListField(ReferenceField('AssignmentModel'))
     
-    meta = {'collection': 'users'}  # Definindo a coleção explicitamente
+    meta = {'collection': 'users'}  
